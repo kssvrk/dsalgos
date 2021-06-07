c6522ece2e04fe36323e11f074596a6c6ad09222
radha krishna <kssvrk@gmail.com>
2021-06-07 09:29:34 +0530

    one pass solution for l-n delete linked list

--- leetcode/linked_list/remove_nth_node_linked_list_19.py
+++ leetcode/linked_list/remove_nth_node_linked_list_19.py
@@ -8,11 +8,12 @@ Created on Sun Jun  6 23:31:30 2021
 
 #19. Remove Nth Node From End of List
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
+  
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         _=0
         if(n==0):
@@ -34,3 +35,26 @@ class Solution:
             if(h):
                 h=h.next
         return head
+#-------------- One pass solution------------------
+class Solution2:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        k=0
+        a=head
+        if(not a):
+            return None
+        while(k<n-1):
+            a=a.next
+            k=k+1
+        #----------a stopped at n-1 index , nth element
+        b=head
+        dh=ListNode()
+        dh.next=b
+        prev=dh
+        #b and a advancing 1 each time till a ends and b should be deleted
+        
+        while(a.next):
+            a=a.next
+            prev=b
+            b=b.next
+        prev.next=b.next
+        return dh.next
